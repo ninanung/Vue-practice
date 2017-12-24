168f0c0ac6681cba8de5f716c49b274ed22694bd
ninanung <ninanung@naver.com>
2017-12-24 21:55:59 +0900

    slot practice

---
+++ scopedslottest/src/components/ScopedSlot.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="child">
+        x : <input type="text" v-model="x"/>
+        y : <input type="text" v-model="y"/>
+        <slot name="type1" :cx="x" :cy="y"></slot>
+        <slot name="type2" :cx="x" :cy="y"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                x: 4,
+                y: 5
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .child {
+        padding: 5px;
+        border: solid 1px gray;
+    }
+</style>
---
+++ slottest/src/components/AppNamed.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="app">
+        <layout>
+            <h1 slot="header">Header Area</h1>
+            <div slot="sidebar">
+                <ul class="menu">
+                    <li v-for="sidebar in sidebars">
+                        <a v-bind:href="sidebar.link">{{ sidebar.menu }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div slot="content">
+                <h2>Content Area</h2>
+                <p>A Premium Noteboo For University And Business. Wirebound Notebook.</p>
+                <p>A Premium Noteboo For University And Business. Wirebound Notebook.</p>
+                <p>A Premium Noteboo For University And Business. Wirebound Notebook.</p>
+                <p>A Premium Noteboo For University And Business. Wirebound Notebook.</p>
+            </div>
+            <p slot="footer">Footer Area</p>
+        </layout>
+    </div>
+</template>
+
+<script>
+    import Layout from "./NamedSlot.vue";
+
+    export default {
+        data: function() {
+            return {
+                sidebars: [
+                    { menu: "hone", link: "#" },
+                    { menu: "about", link: "#"},
+                    { menu: "contact", link: "#"},
+                    { menu: "Vue.js", link: "#"}
+                ]
+            }
+        },
+        components: { Layout }
+    }
+</script>
+
+<style scoped>
+    ul.menu {
+        position: relative;
+        padding: 5px 5px 5px 5px;
+        list-style: none;
+        font-style: italic;
+    }
+    ul.menu a {
+        text-decoration: none;
+    }
+</style>
