213b720d2d8685f2393529393e33a767d1365c06
ninanung <ninanung@naver.com>
2018-01-09 18:50:22 +0900

    dynamic router

---
+++ dynamic-route/src/components/ContactByNo.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1>Contact Detail</h1>
+        <contacts></contacts>
+        <div>
+            <table class="detail table table-bordered">
+                <tbody>
+                    <tr class="active">
+                        <td>no</td>
+                        <td>{{ contact.no }}</td>
+                    </tr>
+                    <tr class="actice">
+                        <td>name</td>
+                        <td>{{ contact.name }}</td>
+                    </tr>
+                    <tr class="active">
+                        <td>tel</td>
+                        <td>{{ contact.tel }}</td>
+                    </tr>
+                </tbody>
+            </table> 
+        </div>
+    </div>
+</template>
+
+<script>
+import contactlist from '../contactlist.js';
+import Contacts from './Contacts.vue';
+
+export default {
+    name: 'contactbyno',
+    components: { Contacts },
+    data: function() {
+        return {
+            no: 0,
+            contacts: contactlist.contacts
+        }
+    },
+    computed: {
+        contact: function() {
+            let no = this.$route.params.no;
+            let arr = this.contacts.filter(function(value, index) {
+                return value.no == no;
+            });
+            if(arr.length == 1) return arr[0];
+            else return {};
+        }
+    }
+}
+</script>
+
+<style>
+    table.detail {
+        width: 400px;
+    }
+</style>
