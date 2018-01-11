168c4b0d80dd51d9ede7ed9979f192ab23ad6563
ninanung <ninanung@naver.com>
2018-01-11 19:09:56 +0900

    named and push router

---
+++ named-route/src/contactlist.js
@@ -0,0 +1,14 @@
+let contactlist = {
+    contacts: [
+        { no: 1, name: "kim", tel: "010-0000-0000" },
+        { no: 2, name: "jun", tel: "010-0000-0000" },
+        { no: 3, name: "lee", tel: "010-0000-0000" },
+        { no: 4, name: "park", tel: "010-0000-0000" },
+        { no: 5, name: "jung", tel: "010-0000-0000" },
+        { no: 6, name: "ahn", tel: "010-0000-0000" },
+        { no: 7, name: "kang", tel: "010-0000-0000" },
+        { no: 8, name: "huh", tel: "010-0000-0000" }
+    ]
+}
+
+export default contactlist;
