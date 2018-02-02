7d08a2a164d158fdc650b25d7b5add40e34cf210
ninanung <ninanung@naver.com>
2018-02-02 22:34:20 +0900

    backend

--- express-vue/backend/routes/login.js
+++ express-vue/backend/routes/login.js
@@ -6,24 +6,25 @@ router.post("/", function(req, res, next) {
     const id = req.body.id;
     const password = req.body.password;
     let info = {
-        error: false,
+        error: "false",
         words: "",
         id: "",
         password: ""
     }
     User.findOne({ id: id }, function(err, user) {
         if(err) {
-            info.error = true;
+            info.error = "true";
             info.words = "Unknown Error Come Out.";
             return res.send(info);
         }
         if(!user) {
-            info.error = true;
+            info.error = "true";
             info.words = "Please Check Your ID or Password";
             return res.send(info);
         }
         info.id = id;
         info.password = password;
+        console.log(info);
         return res.send(info);
     })
 });
--- express-vue/backend/routes/signin.js
+++ express-vue/backend/routes/signin.js
@@ -6,34 +6,35 @@ router.post("/", function(req, res, next) {
     const id = req.body.id;
     const password = req.body.password;
     let info = {
-        error: false,
+        error: "false",
         words: "",
         id: "",
         password: ""
     }
     User.findOne({ id: id }, function(err, user) {
         if(err) {
-            info.error = true;
+            info.error = "true";
             info.words = "Unknown Error Come Out.";
             return res.send(info);
         }
         if(user) {
-            info.error = true;
+            info.error = "true";
             info.words = "Same ID Already Exist.";
             return res.send(info);
         }
         let newUser = new User({
-            id = id,
-            password = password
+            id: id,
+            password: password
         });
         newUser.save(function(err) {
             if(err) {
-                info.error = true;
+                info.error = "true";
                 info.words = "Unknown Error Come Out.";
                 return res.send(info);
             }
             info.id = id;
             info.password = password;
+            console.log(info);
             return res.send(info);
         })
     })
